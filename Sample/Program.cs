87519a5200b5658ba17c4469759101721ffd3fc0
ROBERT_PC <ROBERT_PC@ROBERT-PC1>
2023-02-10 16:16:27 +0800

    Add project files.

---
+++ Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 2, 3, 4 };
+
+            int prod = 1;
+
+            foreach (int i in input)
+            {
+                prod *= i;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = prod / input[i];
+            }
+
+            foreach (int i in input) 
+                Console.WriteLine(i);
+        }
+    }
+}
